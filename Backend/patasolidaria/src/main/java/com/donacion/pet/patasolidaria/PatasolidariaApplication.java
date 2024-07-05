e0790669c5e912b6d034bec25bd39f328519d4be
carlos alecrim <csilva1073@gmail.com>
2024-07-05 18:18:41 -0400

    initial project

---
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/PatasolidariaApplication.java
@@ -0,0 +1,13 @@
+package com.donacion.pet.patasolidaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatasolidariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PatasolidariaApplication.class, args);
+	}
+
+}
---
+++ Backend/patasolidaria/src/test/java/com/donacion/pet/patasolidaria/PatasolidariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donacion.pet.patasolidaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatasolidariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
