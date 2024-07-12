f99a1edd9d8807d519cde8de3e115968e1dec29c
Alejandro Rey Vera <alejoreyv@gmail.com>
2024-07-11 19:28:27 -0500

    Corrigo nombre de Raca a Raza.

--- Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/model/Raza.java
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/model/Raza.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class Raca {
+public class Raza {
 
     private static final long serialVersion = 1L;
 
--- Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/repository/RacaRepository.java
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/repository/RacaRepository.java
@@ -1,7 +1,7 @@
 package com.donacion.pet.patasolidaria.repository;
 
-import com.donacion.pet.patasolidaria.model.Raca;
+import com.donacion.pet.patasolidaria.model.Raza;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface RacaRepository extends JpaRepository<Raca, String> {
+public interface RacaRepository extends JpaRepository<Raza, String> {
 }
