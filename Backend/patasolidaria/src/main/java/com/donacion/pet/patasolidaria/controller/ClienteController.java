9eeba3c9362db11eccc76ccb2ed5f518ef28b6b9
carlos alecrim <csilva1073@gmail.com>
2024-07-13 17:10:32 -0400

    definicion del Controller y del metodo crearCliente

--- Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/controller/ClienteController.java
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/controller/ClienteController.java
@@ -1,4 +1,24 @@
 package com.donacion.pet.patasolidaria.controller;
 
+import com.donacion.pet.patasolidaria.dto.ClienteDTO;
+import com.donacion.pet.patasolidaria.model.Cliente;
+import com.donacion.pet.patasolidaria.service.ClienteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/clientes")
 public class ClienteController {
+
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity<Cliente> crearCliente(@RequestBody ClienteDTO clienteDTO){
+
+        Cliente cliente = clienteService.crearCliente(clienteDTO);
+        return  ResponseEntity.status(HttpStatus.CREATED).body(cliente);
+    }
 }
