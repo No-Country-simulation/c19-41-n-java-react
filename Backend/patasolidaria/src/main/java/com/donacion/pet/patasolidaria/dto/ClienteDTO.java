aa9a60f0496540a641cfcaf4bccccff8c56e9000
carlos alecrim <csilva1073@gmail.com>
2024-07-13 17:17:23 -0400

    creacion del dto cliente

---
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/dto/ClienteDTO.java
@@ -0,0 +1,54 @@
+package com.donacion.pet.patasolidaria.dto;
+
+import com.donacion.pet.patasolidaria.model.Endereco;
+
+import java.util.List;
+
+public class ClienteDTO {
+
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String telefone;
+    private List<Endereco> enderecos;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
+
+    public List<Endereco> getEnderecos() {
+        return enderecos;
+    }
+
+    public void setEnderecos(List<Endereco> enderecos) {
+        this.enderecos = enderecos;
+    }
+}
