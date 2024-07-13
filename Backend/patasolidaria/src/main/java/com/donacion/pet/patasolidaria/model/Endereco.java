26a76445d50222602b83b40e495becf7bf7d1242
carlos alecrim <csilva1073@gmail.com>
2024-07-13 11:02:16 -0400

    añadido relacionamiento con la classe cliente y getters and setters

--- Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/model/Endereco.java
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/model/Endereco.java
@@ -1,12 +1,11 @@
 package com.donacion.pet.patasolidaria.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
 
 @Entity
-public class Endereco {
+public class Endereco implements Serializable {
 
     private static final long serialVersion = 1L;
 
@@ -20,5 +19,58 @@ public class Endereco {
 
     private String cep;
 
+    private String cidade;
+
+    @ManyToOne
+    @JoinColumn(name="cliente_id")
+    private Cliente cliente;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getLogradouro() {
+        return Logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        Logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
 }
 
