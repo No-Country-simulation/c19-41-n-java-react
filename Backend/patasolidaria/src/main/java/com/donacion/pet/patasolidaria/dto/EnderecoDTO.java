839ebdcc8385729271b6ca7a135c3eaa8542b4ba
carlos alecrim <csilva1073@gmail.com>
2024-07-13 17:14:29 -0400

    añadir getters and setters

--- Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/dto/EnderecoDTO.java
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/dto/EnderecoDTO.java
@@ -6,4 +6,36 @@ public class EnderecoDTO {
     private String numero;
     private String cep;
     private String cidade;
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public String getNumero() {
+        return numero;
+    }
+
+    public void setNumero(String numero) {
+        this.numero = numero;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
 }
