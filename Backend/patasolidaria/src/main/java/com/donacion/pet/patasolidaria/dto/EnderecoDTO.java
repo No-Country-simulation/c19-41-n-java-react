66fc927af0d59013540778fb4eb687668b862ca7
carlos alecrim <csilva1073@gmail.com>
2024-07-13 15:24:11 -0400

    creacion de DTO endereco

---
+++ Backend/patasolidaria/src/main/java/com/donacion/pet/patasolidaria/dto/EnderecoDTO.java
@@ -0,0 +1,9 @@
+package com.donacion.pet.patasolidaria.dto;
+
+public class EnderecoDTO {
+
+    private String logradouro;
+    private String numero;
+    private String cep;
+    private String cidade;
+}
