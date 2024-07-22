f47a79288cbeba6464acab7c5961ad005573eea1
carlos alecrim <csilva1073@gmail.com>
2024-07-22 17:54:12 -0400

    create component section

---
+++ Frontend/patasolidaria/src/components/home/HomeSection.jsx
@@ -0,0 +1,47 @@
+
+export default function HomeSection(){
+
+    return(
+        <>
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <div class="card">
+                            <div class="card-header">
+                                Featured
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                    <div class="card">
+                            <div class="card-header">
+                                Featured
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">Special title treatment</h5>
+                                <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                            </div>
+                    </div>
+                </div>
+                <div className="col">
+
+                <div class="card">
+                    <div class="card-header">
+                        Featured
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Special title treatment</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        </>
+    );
+}
